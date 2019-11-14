c50683867a2c98a51452c1e93c08dc50709817ac
pydigger <roberto.gil.up@gmail.com>
2019-11-14 07:00:06 -0800

    build production

---
+++ build/precache-manifest.30e413a21e26249542dd572ac36247ea.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cd65419a2700d28fbfe899a056f1369",
+    "url": "./index.html"
+  },
+  {
+    "revision": "02127cf998e4d6601551",
+    "url": "./static/css/main.3d2812db.chunk.css"
+  },
+  {
+    "revision": "02127cf998e4d6601551",
+    "url": "./static/js/main.828b756a.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
